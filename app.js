a1bf1878e39cf3bd0efb688bbe23438f940ee4ad
Shaurya-dub <shauryadubey@hotmail.ca>
2021-07-07 20:01:52 -0400

    made hamburger menu more accessible and added transitions

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const hamburger = document.querySelector(".fa-bars");
-const menu = document.querySelector('.mobileMenu');
 
-hamburger.addEventListener('click', function() {
-    menu.classList.toggle('open');
-})
+    
+    const menuBtn = document.querySelector('.menuBtn');
+    const menu = document.querySelector('.mobileMenu');
+// Defining functions to open/close menu
+    const toggleHamburgerOpen = () => {
+      menuBtn.classList.toggle("move");
+      menu.classList.toggle("open");
+    }
+
+    const closeHamburgerMenu = () => {
+      menuBtn.classList.remove("move");
+      menu.classList.remove("open");
+    }
+
+// Event Listeners for when hamburger is clicked/pressed enter upon
+menuBtn.addEventListener('click', toggleHamburgerOpen);
+
+menuBtn.addEventListener('focusout', closeHamburgerMenu);
+
+menuBtn.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+  toggleHamburgerOpen();
+    }
+});
+
+// To close the menu if page is resized
+window.addEventListener('resize', closeHamburgerMenu )
