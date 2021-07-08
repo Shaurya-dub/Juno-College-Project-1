5f0747657feb4a946bdd3b522f4b84b1dabfabc4
Shaurya-dub <shauryadubey@hotmail.ca>
2021-07-07 21:14:08 -0400

    fixed css a bit to acommodate new menu

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
     
     const menuBtn = document.querySelector('.menuBtn');
     const menu = document.querySelector('.mobileMenu');
+    const menuHolder = document.querySelector('.mobileMenuHolder');
 // Defining functions to open/close menu
     const toggleHamburgerOpen = () => {
       menuBtn.classList.toggle("move");
@@ -16,13 +17,13 @@
 // Event Listeners for when hamburger is clicked/pressed enter upon
 menuBtn.addEventListener('click', toggleHamburgerOpen);
 
-menuBtn.addEventListener('focusout', closeHamburgerMenu);
 
 menuBtn.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-  toggleHamburgerOpen();
-    }
+  if (e.keyCode === 13) {
+    toggleHamburgerOpen();
+  }
 });
 
 // To close the menu if page is resized
 window.addEventListener('resize', closeHamburgerMenu )
+
