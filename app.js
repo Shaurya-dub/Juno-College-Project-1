d4de824427c219bc432e9339176a66cc74a880d4
Shaurya-dub <shauryadubey@hotmail.ca>
2022-01-19 14:52:23 -0500

    minor responsive changes

--- app.js
+++ app.js
@@ -1,29 +1,37 @@
+const menuBtn = document.querySelector(".menuBtn");
+const menu = document.querySelector(".mobileMenu");
+const menuHolder = document.querySelector(".mobileMenuHolder");
 
-    
-    const menuBtn = document.querySelector('.menuBtn');
-    const menu = document.querySelector('.mobileMenu');
-    const menuHolder = document.querySelector('.mobileMenuHolder');
 // Defining functions to open/close menu
-    const toggleHamburgerOpen = () => {
-      menuBtn.classList.toggle("move");
-      menu.classList.toggle("open");
-    }
+const toggleHamburgerOpen = (e) => {
+  e.stopImmediatePropagation();
+  menuBtn.classList.toggle("move");
+  menu.classList.toggle("open");
+};
 
-    const closeHamburgerMenu = () => {
-      menuBtn.classList.remove("move");
-      menu.classList.remove("open");
-    }
+const closeHamburgerMenu = () => {
+  menuBtn.classList.remove("move");
+  menu.classList.remove("open");
+};
+
+// To close the menu if page is resized
+window.addEventListener("resize", closeHamburgerMenu);
+
+window.addEventListener("click", function (e) {
+  if (
+    e.target.className !== "menuBtn" &&
+    e.target.className !== "menuBtnBurger" &&
+    menu.className !== "open"
+  ) {
+    closeHamburgerMenu();
+  }
+});
 
 // Event Listeners for when hamburger is clicked/pressed enter upon
-menuBtn.addEventListener('click', toggleHamburgerOpen);
+menuBtn.addEventListener("click", toggleHamburgerOpen);
 
-
-menuBtn.addEventListener('keyup', (e) => {
+menuBtn.addEventListener("keyup", (e) => {
   if (e.keyCode === 13) {
     toggleHamburgerOpen();
   }
 });
-
-// To close the menu if page is resized
-window.addEventListener('resize', closeHamburgerMenu )
-
